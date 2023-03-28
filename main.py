ad0ca5ec70fc10f0b5f0d8dc1b9d15d917be5a2c
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2023-03-28 13:55:52 +0000

    Add conditionals to give message based off of score.

--- main.py
+++ main.py
@@ -25,3 +25,11 @@ v = lowercase_couples_string.count("v")
 e = lowercase_couples_string.count("e")
 
 love = l + o + v + e
+
+love_score = str(true) + str(love)
+
+#Message based of result
+if (love_score < 10) or (love_score > 90):
+  print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif (love_score >= 40) and (love_score <= 50):
+  print(f"Your score is {love_score}, you are alright together.")
